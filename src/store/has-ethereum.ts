0b0073d889546212734ee9790408d411c88c44ef
aggre <hiroyuki.aggre@gmail.com>
2019-11-30 15:53:23 +0900

    move declaration to d

--- src/store/has-ethereum.ts
+++ src/store/has-ethereum.ts
@@ -1,18 +1,3 @@
 import { BehaviorSubject } from 'rxjs'
-import { HttpProvider } from 'web3-core'
-import Web3 from 'web3'
-
-interface Ethereum extends HttpProvider {
-	enable: () => Promise<void>
-	selectedAddress: string | null
-	isConnected: () => boolean
-}
-
-declare global {
-	interface Window {
-		ethereum: Ethereum
-		Web3: new (prop?: Ethereum) => Web3
-	}
-}
 
 export const hasEthereum = new BehaviorSubject(false)
