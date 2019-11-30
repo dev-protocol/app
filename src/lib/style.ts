62404af8e0f3f9e89afc97910b954c090e0e43d3
aggre <hiroyuki.aggre@gmail.com>
2019-11-30 14:45:57 +0900

    removed postcss-preset-env because so over spec it

--- src/lib/style.ts
+++ src/lib/style.ts
@@ -1,11 +1,10 @@
 import { process, directive } from 'lit-style'
 import { until } from 'lit-html/directives/until'
 import { html } from 'lit-html'
-import postcssPresetEnv from 'postcss-preset-env'
 import cssnanano from 'cssnano'
 
 export const processor = process({
-	plugins: [postcssPresetEnv({ stage: 0 }), cssnanano({ preset: 'default' })]
+	plugins: [cssnanano({ preset: 'default' })]
 })
 
 export const style = directive(
