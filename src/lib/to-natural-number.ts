15e9f5a4640853d9c2c18e7353ba84066f1f52ef
aggre <hiroyuki.aggre@gmail.com>
2020-01-27 15:23:32 +0900

    fix spec

--- src/lib/to-natural-number.ts
+++ src/lib/to-natural-number.ts
@@ -3,4 +3,4 @@ import BigNumber from 'bignumber.js'
 const basis = new BigNumber(10).pow(18)
 
 export const toNaturalNumber = (num: string): BigNumber =>
-	new BigNumber(num).times(basis)
+	new BigNumber(num).div(basis)
