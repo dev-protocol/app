b8b591c0ad1e85f97cbe3a9e0bbd4ba087a6ab59
aggre <hiroyuki.aggre@gmail.com>
2020-02-02 01:17:49 +0900

    shown open button

--- src/component/for-lp/card.ts
+++ src/component/for-lp/card.ts
@@ -1,5 +1,5 @@
 import { DirectiveFunction, component, subscribe } from '@aggre/ullr/directive'
-import { html } from 'lit-html'
+import { html, svg } from 'lit-html'
 import { style } from '../../lib/style'
 import { DevProperty } from '../../store/properties'
 import { asVar } from '../../lib/style-properties'
@@ -98,7 +98,7 @@ const updateStore = (store: Store, address: string): Store => {
 	return store
 }
 
-const stakeButton = html`
+const stakeButton = svg`
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		fill="none"
@@ -108,6 +108,23 @@ const stakeButton = html`
 	>
 		<circle fill="${asVar('secondaryColor')}" cx="12" cy="12" r="12" />
 		<path stroke="white" stroke-linecap="round" d="M 9 6 L 15 12 L 9 18" />
+	</svg>
+`
+
+const openButton = svg`
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		width="100%"
+		height="100%"
+	>
+		<circle cx="12" cy="12" r="12" fill="#4caf50" />
+		<path
+			d="M7 6.5H17C17.8284 6.5 18.5 7.17157 18.5 8V15C18.5 15.8284 17.8284 16.5 17 16.5H7C6.17157 16.5 5.5 15.8284 5.5 15V8C5.5 7.17157 6.17157 6.5 7 6.5Z"
+			stroke="white"
+		/>
+		<path d="M18 7L12 13L6 7" stroke="white" stroke-linecap="round" />
 	</svg>
 `
 
@@ -204,10 +221,19 @@ export const card = ({
 								</dl>
 							</div>
 							<div class="button">
-								<div>
-									${stakeButton}
-									<p>Stake</p>
-								</div>
+								${amounts?.account?.isGreaterThanOrEqualTo(1)
+									? html`
+											<div>
+												${openButton}
+												<p>Open</p>
+											</div>
+									  `
+									: html`
+											<div>
+												${stakeButton}
+												<p>Stake</p>
+											</div>
+									  `}
 							</div>
 						</div>
 					`)}
