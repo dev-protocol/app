1de6f54802fd74d6b70eaf5b6fa3c8dbcbe9da11
aggre <hiroyuki.aggre@gmail.com>
2020-02-10 16:45:37 +0900

    update test

--- src/component/pure/section.test.ts
+++ src/component/pure/section.test.ts
@@ -21,3 +21,18 @@ test('Returns template for <section> element', t => {
 	const el = document.body.querySelector('section > p') as HTMLElement
 	t.is(removeExtraString(el.innerHTML), 'Test')
 })
+
+test('Returns template for <section> element with ID when passed 2nd args', t => {
+	render(
+		section(
+			() =>
+				html`
+					<p>Test</p>
+				`,
+			'test'
+		),
+		document.body
+	)
+	const el = document.body.querySelector('section#test > p') as HTMLElement
+	t.is(removeExtraString(el.innerHTML), 'Test')
+})
